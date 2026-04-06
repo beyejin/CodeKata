d73038e2a35d4300831b468b8799435060058e48
hanyejin <biziyejin8844@naver.com>
2026-04-06 09:24:40 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(0 < angle & angle < 90){
+            answer = 1;
+        }
+        else if(angle == 90){
+            answer = 2;
+        }
+        else if(90 < angle & angle < 180){
+            answer = 3;
+        }
+        else if( angle == 180){
+            answer = 4;
+        }
+        else{
+            System.out.println("잘못된 입력값입니다");
+        }
+        return answer;
+    }
+}
