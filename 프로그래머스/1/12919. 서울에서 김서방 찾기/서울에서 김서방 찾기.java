7f28d665960da345bdf0d058239abbba74e4e8ea
hanyejin <biziyejin8844@naver.com>
2026-04-10 09:42:51 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.27 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,16 +1,11 @@
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "김서방은 ";
+     
         for(int i = 0; i < seoul.length; i++){
             if(seoul[i].equals("Kim")){
-                answer += String.valueOf(i);
-                break;
-            }
-            else{
-                continue;
+                return "김서방은 " + i + "에 있다";
             }
         }
-        answer+="에 있다";
-        return answer;
+        return "";
     }
 }
