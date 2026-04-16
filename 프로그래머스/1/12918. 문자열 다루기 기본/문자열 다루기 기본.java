13da0f11462b4feed475031398704ad25f12bfa7
hanyejin <biziyejin8844@naver.com>
2026-04-16 10:11:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        
+        char[] arr = s.toCharArray();
+        
+        if(s.length() != 4 && s.length() != 6){
+            return false;
+        }
+        
+        for(int i =0; i<s.length(); i++){
+            if(!Character.isDigit(arr[i])){
+                return false;
+            } 
+        }
+        return true;
+    }
+}
