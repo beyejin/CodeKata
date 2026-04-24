2977a3d2733cd56bb130c545cb2412ca2672e23c
hanyejin <biziyejin8844@naver.com>
2026-04-24 09:48:50 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.38 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,17 +1,30 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        
-        Arrays.sort(strings,(s1,s2)->{
-            if(s1.charAt(n)==s2.charAt(n)){
-                return s1.compareTo(s2);
+        int a = strings.length;
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a - i - 1; j++) {
+
+                // n번째 문자가 더 크면 자리 교체
+                if (strings[j].charAt(n) > strings[j + 1].charAt(n)) {
+                    String change = strings[j];
+                    strings[j] = strings[j + 1];
+                    strings[j + 1] = change;
+                }
+
+                // n번째 문자가 같으면 사전순 비교
+                else if (strings[j].charAt(n) == strings[j + 1].charAt(n)) {
+                    if (strings[j].compareTo(strings[j + 1]) > 0) {
+                        String change = strings[j];
+                        strings[j] = strings[j + 1];
+                        strings[j + 1] = change;
+                    }
+                }
             }
-            else{
-                return s1.charAt(n)-s2.charAt(n);
-            }
-        });
+        }
+
         return strings;
-    } 
+    }
 }
