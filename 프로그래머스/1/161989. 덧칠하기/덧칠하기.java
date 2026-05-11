d16ede7dfc1f488a59501dba3d3dcdaf0ad32c26
hanyejin <biziyejin8844@naver.com>
2026-05-11 09:39:01 +0900

    [level 1] Title: 덧칠하기, Time: 1.40 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int index =0;
+        
+        for(int s :section){
+            if(s>index){
+                answer++;
+                index= m+s-1;
+            }
+        }
+        return answer;
+    }
+}
